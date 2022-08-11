203a79e3174e0436ee7f3cd890c937303287d0d5
saviosa <saviosilvadearaujo2019@gmaiil.com>
2022-08-11 20:59:19 -0300

    feat(input-search.component): add input search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    InputSearchComponent,
   ],
   imports: [
     BrowserModule
